8d19133903ccd533bb4ed61eedcd9ff6049d1479
nmorrone <n.morrone96@gmail.com>
2024-10-07 17:07:01 +0200

    check.to.string.methods

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,14 +5,6 @@ public class Main {
 	
 public static void main(String[]args) {
 	
-	Cuffie airpods = new Cuffie("airpods", "cuffie professionali wireless", 349.00);
-	airpods.setWireless(true);
-
-	System.out.println(airpods.getCodice());
-	System.out.println(airpods.getIva());
-	System.out.println(airpods.toStringCuffie());
-	
-	
 	Prodotto prodotto1 = new Prodotto();
 	prodotto1.setNome("Playstation 5");
 	prodotto1.setDescrizione("Console videogiochi di ultima generazione");
@@ -21,12 +13,23 @@ public static void main(String[]args) {
 	
 	System.out.println(prodotto1.toString());
 	
+	Cuffie airpods = new Cuffie("airpods", "cuffie professionali wireless", 349.00);
+	airpods.setWireless(true);
+	airpods.setColore("Bianco");
+	System.out.println(airpods.toStringCuffie());
+	
 	Televisori televisore = new Televisori("Samsung Smart TV","Nuova televisione di ultima generazione", 699.00);
 	televisore.setLarghezza(135);
 	televisore.setAltezza(80);
 	televisore.setProfondita(5);
 	televisore.setSmart(true);
 	System.out.println(televisore.toStringTv());
+	
+	Smartphones smartphone = new Smartphones("iPhone 13 Pro", "Con nuova fotocamera grandangolare e intelligenza AI", 1199.00);
+	smartphone.setImei("049922294271648930");
+	smartphone.setMemoria(5120);
+	System.out.println(smartphone.toStringSmart())
+;	
 }
 
 }
